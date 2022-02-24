ad6e3bb4c36c3e67559e5b573e0a72467f54deff
Rafaela Jordão <rafa.jordao.07@hotmail.com>
2022-02-24 19:07:01 -0300

    requisito 5

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ class App extends React.Component {
     this.state = {
       inputName: '',
       description: '',
-      firstAttribute: '',
-      secondAttribute: '',
-      thirdAttribute: '',
+      firstAttribute: 0,
+      secondAttribute: 0,
+      thirdAttribute: 0,
       image: '',
       inputRare: 'Normal',
       trunfo: false,
@@ -24,6 +24,52 @@ class App extends React.Component {
     this.setState({ [name]: value });
   }
 
+  onSaveButtonClick = () => {
+    this.setState({
+      inputName: '',
+      description: '',
+      firstAttribute: 0,
+      secondAttribute: 0,
+      thirdAttribute: 0,
+      image: '',
+      inputRare: 'Normal',
+    });
+  }
+
+  validateBtn = () => {
+    const { inputName, description, firstAttribute,
+      secondAttribute, thirdAttribute, image } = this.state;
+
+    const attr1 = parseInt(firstAttribute, 10);
+    const attr2 = parseInt(secondAttribute, 10);
+    const attr3 = parseInt(thirdAttribute, 10);
+    const sum = attr1 + attr2 + attr3;
+
+    const valueMin = 0;
+    const valueMax = 90;
+    const limit = 210;
+
+    if (inputName === '' || description === '' || image === '') {
+      return true;
+    }
+
+    if (attr1 < valueMin || attr1 > valueMax) {
+      return true;
+    }
+
+    if (attr2 < valueMin || attr2 > valueMax) {
+      return true;
+    }
+
+    if (attr3 < valueMin || attr3 > valueMax) {
+      return true;
+    }
+
+    if (sum > limit) {
+      return true;
+    }
+  }
+
   render() {
     const { inputName, description, firstAttribute, secondAttribute, thirdAttribute,
       image, inputRare, trunfo } = this.state;
@@ -32,6 +78,8 @@ class App extends React.Component {
       <div>
         <Form
           onInputChange={ this.handleChange }
+          onSaveButtonClick={ this.onSaveButtonClick }
+          isSaveButtonDisabled={ this.validateBtn() }
           cardName={ inputName }
           cardDescription={ description }
           cardAttr1={ firstAttribute }
